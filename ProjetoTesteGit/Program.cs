29a649e25a04cb052ed1fc076a23bbe76a6324f6
Denis Fioravante <denisfioravante722@hotmail.com>
2018-12-31 19:46:07 -0200

    Projeto Iniciado com Ola Mundo

---
+++ ProjetoTesteGit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjetoTesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá Mundo!!");
+            Console.WriteLine("Hello Git");
+            Console.ReadLine();
+        }
+    }
+}
